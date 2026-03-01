7e1e86bfdb5c950407e8fee84ce85f883be2f282
Abdelrahman Eleewa <a.eleewa.m@gmail.com>
2026-03-01 22:43:52 +0200

    First test push

---
+++ lib/feature/parent_auth/presentation/views/test.dart
@@ -0,0 +1,18 @@
+// This is my first test push
+
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello'),
+      color: Colors.red,
+      height: 200,
+      width: 200,
+      padding: EdgeInsets.all(20),
+    );
+  }
+}
