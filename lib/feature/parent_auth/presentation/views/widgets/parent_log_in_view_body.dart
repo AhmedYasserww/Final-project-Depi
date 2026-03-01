f216b9dddfefe5fb31277b1887186e48523aade4
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2026-03-01 21:45:53 +0200

    add font style inter to this project

--- lib/feature/parent_auth/presentation/views/widgets/parent_log_in_view_body.dart
+++ lib/feature/parent_auth/presentation/views/widgets/parent_log_in_view_body.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:kids_education_learning/core/utils/app_style.dart';
 class ParentLogInViewBody extends StatelessWidget {
   const ParentLogInViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('ParentLogInViewBody'),);
+    return  Center(child: Text('ParentLogInViewBody',
+      style: AppStyle.styleSemiBold20,),);
   }
 }
